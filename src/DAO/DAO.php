6b28c3dc3b39647d1eba4547d056ed8e3f6545ad
Hakim-LMS <hlouahem@gmail.com>
2017-07-07 11:34:41 +0200

    Del model.php -> gestion a la connection de DB par les DAO

---
+++ src/DAO/DAO.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace MicroCMS\DAO;
+
+use Doctrine\DBAL\Connection;
+
+abstract class DAO{
+    private $db; 
+    
+    public function __construct(Connection $db)
+    {
+        $this->db = $db;
+    }
+    
+     protected function getDb() {
+        return $this->db;
+    }
+    
+    /**
+     * Builds a domain object from a DB row.
+     * Must be overridden by child classes.
+     */
+    protected abstract function buildDomainObject(array $row);
+}
