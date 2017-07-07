67e230f4376b286fdfbfb7e0c4066f6bccf4a8eb
Hakim-LMS <hlouahem@gmail.com>
2017-07-07 11:37:00 +0200

    switch PDO-> Doctrine DBAL + MAJ db

--- db/Structure.sql
+++ db/Structure.sql
@@ -8,6 +8,7 @@
  * Created: 23 juin 2017
  */
 drop table if exists t_articles;
+drop table if exists t_comment;
 
 create table t_articles
 (
@@ -15,3 +16,11 @@ art_id integer not null primary key auto_increment,
 art_title varchar (100) not null, 
 art_content varchar (2000) not null)
 engine=innodb character set utf8 collate utf8_unicode_ci;
+
+create table t_comment (
+    com_id integer not null primary key auto_increment,
+    com_author varchar(100) not null,
+    com_content varchar(500) not null,
+    art_id integer not null,
+    constraint fk_com_art foreign key(art_id) references t_articles(art_id)
+) engine=innodb character set utf8 collate utf8_unicode_ci;
